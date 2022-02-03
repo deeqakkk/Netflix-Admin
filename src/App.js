1c5b9762e12e484d9c2e73b976ff95c895675af8
Deepak <0822deepakverma@gmail.com>
2022-02-03 17:30:10 +0530

    sidebar initiated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-
-import React from 'react';
+import Topbar from "./Components/Topbar/Topbar.jsx";
+import Sidebar from "./Components/Sidebar/Sidebar.jsx";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./app.css";
 
 export default function App() {
-  return <div className='App'>Hello</div>;
+  return (
+    <div className="App">
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+        <div className="others">other pages</div>
+      </div>{" "}
+    </div>
+  );
 }
-
