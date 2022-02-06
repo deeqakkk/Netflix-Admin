4b15e2e79bbd2a54d58cbfabe58758589afa171e
Deepak <0822deepakverma@gmail.com>
2022-02-06 13:20:14 +0530

    rechart implemented

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,13 @@
-import React from 'react';
-import "./home.css"
-import FeaturedInfo from '../../Components/FeaturedInfo/FeaturedInfo';
+import React from "react";
+import "./home.css";
+import FeaturedInfo from "../../Components/FeaturedInfo/FeaturedInfo";
+import Chart from "../../Components/Chart/Chart";
+import {userData }from "../../dummyData";
 export default function Home() {
-  return <div className='home'><FeaturedInfo/></div>;
+  return (
+    <div className="home">
+      <FeaturedInfo />
+      <Chart data={userData} title="User Analytics" grid dataKey="Active User" />
+    </div>
+  );
 }
---
+++ src/dummyData.js
@@ -0,0 +1,49 @@
+export const userData = [{
+        name: "Jan",
+        "Active User": 1000,
+    },
+    {
+        name: "Feb",
+        "Active User": 2000,
+    },
+    {
+        name: "Mar",
+        "Active User": 3000,
+    },
+    {
+        name: "Apr",
+        "Active User": 1000,
+    },
+    {
+        name: "May",
+        "Active User": 5000,
+    },
+    {
+        name: "June",
+        "Active User": 6000,
+    },
+    {
+        name: "Jul",
+        "Active User": 3000,
+    },
+    {
+        name: "Aug",
+        "Active User": 2000,
+    },
+    {
+        name: "Sep",
+        "Active User": 7000,
+    },
+    {
+        name: "Oct",
+        "Active User": 5000,
+    },
+    {
+        name: "Nov",
+        "Active User": 4000,
+    },
+    {
+        name: "Dec",
+        "Active User": 3000,
+    },
+];
