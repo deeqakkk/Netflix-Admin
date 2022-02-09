a03bb037ca972dd4a766bf4d8a3693c1dd336e2b
Deepak <0822deepakverma@gmail.com>
2022-02-09 13:34:12 +0530

    widgets added to the homepage

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,11 +3,17 @@ import "./home.css";
 import FeaturedInfo from "../../Components/FeaturedInfo/FeaturedInfo";
 import Chart from "../../Components/Chart/Chart";
 import {userData }from "../../dummyData";
+import WidgetSm from "../../Components/WidgetSm/WidgetSm";
+import WidgetLg from "../../Components/WidgetLg/WidgetLg";
 export default function Home() {
   return (
     <div className="home">
       <FeaturedInfo />
       <Chart data={userData} title="User Analytics" grid dataKey="Active User" />
+      <div className="homeWidgets">
+        <WidgetSm/>
+        <WidgetLg/>
+      </div>
     </div>
   );
 }
